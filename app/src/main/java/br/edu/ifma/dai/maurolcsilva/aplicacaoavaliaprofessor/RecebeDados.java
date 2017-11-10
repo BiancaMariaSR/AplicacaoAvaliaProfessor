f8ab333a0343e5bae9ee08f27e328d8b04dc703b
Mauro Silva <maurosilva@ifma.edu.br>
2017-11-10 15:05:46 -0200

    4eAtualização.

---
+++ app/src/main/java/br/edu/ifma/dai/maurolcsilva/aplicacaoavaliaprofessor/RecebeDados.java
@@ -0,0 +1,13 @@
+package br.edu.ifma.dai.maurolcsilva.aplicacaoavaliaprofessor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecebeDados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recebe_dados);
+    }
+}
