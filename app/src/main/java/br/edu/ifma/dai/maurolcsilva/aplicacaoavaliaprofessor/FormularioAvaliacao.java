f60d7f985add51d36f69234c56445807acbf79e7
Mauro Silva <maurosilva@ifma.edu.br>
2017-11-14 21:50:15 -0200

    Nova Atualização.

--- app/src/main/java/br/edu/ifma/dai/maurolcsilva/aplicacaoavaliaprofessor/FormularioAvaliacao.java
+++ app/src/main/java/br/edu/ifma/dai/maurolcsilva/aplicacaoavaliaprofessor/FormularioAvaliacao.java
@@ -2,13 +2,24 @@ package br.edu.ifma.dai.maurolcsilva.aplicacaoavaliaprofessor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class FormularioAvaliacao extends AppCompatActivity {
 
+    private Spinner spnDisciplinas;
+    private Spinner spnProfessores;
+    private EditText txtAulas;
+    private RadioGroup rdgNotas;
+    private CheckBox chkObs01;
+    private CheckBox chkObs02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_avaliacao);
         //Aqui iniciaremos nossa implementação
+        spnDisciplinas = (Spinner) findViewById(R.id.spnFormularioDisciplina);
     }
 }
